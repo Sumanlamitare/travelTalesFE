4e8442ac4591bc7793a7f99981e94cc4980880b6
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-22 17:22:50 -0500

    add logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,10 @@ import "./components.css";
 export default function Nav() {
   return (
     <div className="navBar">
-      <img src="" alt="Logo" />
+      <div className="logo">
+        <img src="../assets/TALES TRAVEL.png" alt="Logo" />
+      </div>
+
       <div className="links">
         <h5>Favorites</h5>
         <h5> Add </h5>
