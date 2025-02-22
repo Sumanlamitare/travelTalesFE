ceb68347ca92261e137cdc30fc741f9663d9aa9e
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-22 16:53:21 -0500

    add files for components

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Nav from "../src/components/Nav.jsx";
+
+import "./App.css";
+
+function App() {
+  return <Nav></Nav>;
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import "./components.css";
+
+export default function Nav() {
+  return (
+    <div className="navBar">
+      <img src="" alt="Logo" />
+      <h3>Favorites</h3>
+      <h3> Add </h3>
+    </div>
+  );
+}
