00251bbf9721b3d06f2e0cb9c6db464cc7591d25
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-22 17:47:11 -0500

    add logo and style to the logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,16 @@
 import "./components.css";
+import logo from "../assets/logo.jpg";
 
 export default function Nav() {
   return (
     <div className="navBar">
       <div className="logo">
-        <img src="../assets/TALES TRAVEL.png" alt="Logo" />
+        <img src={logo} alt="Logo" />
       </div>
 
       <div className="links">
-        <h5>Favorites</h5>
-        <h5> Add </h5>
+        <h4>Favorites</h4>
+        <h4> Add </h4>
       </div>
     </div>
   );
