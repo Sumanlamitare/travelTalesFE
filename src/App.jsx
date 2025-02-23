92a96b55fb96f0d3d9f060830f6ad60f9154a883
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-22 22:25:55 -0500

    add necessary file and import logo picture for nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import Nav from "../src/components/Nav.jsx";
-
+import AddLogForm from "./components/AddForm.jsx";
 import "./App.css";
 
 function App() {
-  return <Nav></Nav>;
+  return (
+    <>
+      {" "}
+      <Nav></Nav>
+      <AddLogForm></AddLogForm>
+    </>
+  );
 }
 
 export default App;
