52d30658c4a66cd395546f4dda6c383bf876b2de
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-23 15:44:16 -0500

    add route for favorites

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../src/pages/Home.jsx";
 import LogCard from "./components/LogCard.jsx";
 import Details from "./pages/Details.jsx";
+import Favorite from "./pages/Favorite.jsx";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:log_id" element={<Details />} />
+        <Route path="/favorites" element={<Favorite />}></Route>
       </Routes>
     </>
   );
